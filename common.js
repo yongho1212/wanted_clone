099eb472a82beca2b84720217ccf4280fd052fc2
yongho <yongho6800@gmail.com>
2023-04-13 14:53:05 +0900

    loading indicator

--- common.js
+++ common.js
@@ -16,14 +16,29 @@ class Loading extends HTMLElement{
         container.style.top = "50%";
         container.style.transform = "translate(-50%, -50%)";
         container.style.zIndex = "9999";
-        container.style.backgroundColor = "red";
-        this.appendChild(container)
+        container.style.backgroundColor = "opacity"; // 흐리게 보이는 배경
 
-        let txt = document.createElement('div')
-        txt.classList.add('loadingImg')
-        txt.innerHTML = `<img src="${this.link}">`
-        container.appendChild(txt)
-        console.log(this.link, this)
+        let img = document.createElement('img'); // 이미지 태그 생성
+        img.src = this.link;
+        img.classList.add('loadingImg');
+        img.style.animation = 'rotation 3s infinite linear'; // 3초 동안 회전하는 애니메이션
+
+        container.appendChild(img);
+        this.appendChild(container);
+
+        // 3초 후 로딩 이미지 제거
+        setTimeout(() => {
+            container.style.display = 'none';
+        }, 3000);
+        
+        // this.appendChild(container)
+
+        // let txt = document.createElement('div')
+        // txt.classList.add('loadingImg')
+        // txt.innerHTML = `<img src="${this.link}">`
+        // container.appendChild(txt)
+        // txt.style.transform = 'rotate(720deg)'
+        // console.log(this.link, this)
     }
 }
 customElements.define('loading-img', Loading)
